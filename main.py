d3f3f545ddcd922804cb708aa098ee8497efe191
Sergey9911 <kormilin.sergey@gmail.com>
2024-10-07 23:11:25 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+name=input("my_string: ")
+print(name. __len__())#это подсмотрел в интернете
+print("my_string" .upper())
+print("my_string" .lower())
+print("my"+"string")
+print("my-string"[0])
+print("my_string"[8])
